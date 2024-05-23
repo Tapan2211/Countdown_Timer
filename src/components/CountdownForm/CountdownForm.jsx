eacd9ce29cd0974e4c731a06e6f814be575ffdcb
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-05-23 12:00:56 +0530

    create components folder in necessery jsx and css files

---
+++ src/components/CountdownForm/CountdownForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './CountdownForm.module.css';
+
+function CountdownForm() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CountdownForm;
---
+++ src/components/TimerDisplay/TimerDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './TimerDisplay.module.css';
+
+function TimerDisplay() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TimerDisplay;
